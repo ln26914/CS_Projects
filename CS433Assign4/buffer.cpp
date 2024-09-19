b7fa247f065484435afa4e1912fcb80c489f96b5
Noah Boyd <121831326+ln26914@users.noreply.github.com>
2024-09-19 12:22:25 -0700

    Uploaded Files: Producer / Consumer Problem
    Course: CS 433 (Operating Systems)
    Date Range: Spring 2024

---
+++ CS433Assign4/buffer.cpp
@@ -0,0 +1,96 @@
+/**
+ * Assignment 4: Producer Consumer Problem
+ * @file buffer.cpp
+ * @author Noah Boyd and Sebastian Rojas
+ * @brief Implementation file for the buffer class
+ * @version 0.1
+ */
+
+// You must complete the all parts marked as "TODO". Delete "TODO" after you are
+// done. Remember to add sufficient and clear comments to your code
+#include "buffer.h"
+#include <iostream>
+using namespace std;
+
+// Implemented Buffer Class Here
+
+/*
+Developer Noah here! This file appears to compile without issue! As for logic
+problems, that will be revealed when we develop main.cpp
+
+Send me a discord message at shinespark_ if you need my help with anything!
+*/
+
+// Constructor
+Buffer::Buffer(int size) {
+  this->size = size;
+  this->count = 0;
+  this->front = 0;
+  this->back = -1;
+  this->buffer[0] = 0;
+}
+
+// Destructor to remove item in the buffer
+Buffer::~Buffer() {
+
+  buffer_item removable;
+
+  while (!is_empty()) {
+    remove_item(&removable);
+  }
+}
+// Insert Item
+bool Buffer::insert_item(buffer_item item) {
+
+  // If the buffer is full, return false since no more items can be inserted
+  // into buffer
+  if (is_full()) {
+    return false;
+  }
+  // Push item into the buffer
+  // Increment Count
+  back = (back + 1) % size;
+  buffer[back] = item;
+  count++;
+  return true;
+}
+
+// Remove Item. Removes first instance of parameter
+bool Buffer::remove_item(buffer_item *item) {
+  // If the buffer is empty, return false since nothing can be removed from
+  // buffer
+  if (is_empty()) {
+    return false;
+  }
+  *item = buffer[front];
+  front = (front + 1) % size;
+  count--; // Decrement Count
+  return true;
+}
+
+// Get Buffer Size
+int Buffer::get_size() { return this->size; }
+
+// Get Item Count
+int Buffer::get_count() { return this->count; }
+
+// Is Buffer Empty?
+bool Buffer::is_empty() { return this->count == 0; }
+
+// Is Buffer Full?
+bool Buffer::is_full() { return this->count == this->size - 1; }
+
+// Print Buffer
+void Buffer::print_buffer() {
+  // print function that allows for the buffer to be shown in the format of the
+  // example_out.txt
+  cout << "[";
+  for (int i = front, elements = 0; elements < count;
+       i = (i + 1) % size, ++elements) {
+    if (elements > 0) {
+      cout << ", ";
+    }
+    cout << buffer[i];
+  }
+  cout << "]\n";
+}
---
+++ CS433Assign4/main.cpp
@@ -0,0 +1,102 @@
+/**
+ * Assignment 4: Producer Consumer Problem
+ * @file main.cpp
+ * @author Noah Boyd and Sebastian Rojas
+ * @brief The main program for the producer consumer problem.
+ * @version 0.1
+ */
+// You must complete the all parts marked as "TODO". Delete "TODO" after you are
+// done.
+//  Remember to add sufficient and clear comments to your code
+#include "buffer.h"
+#include <iostream>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+using namespace std;
+
+// global buffer object
+Buffer buffer;
+pthread_mutex_t mutex;
+sem_t full;  // semaphore for count when full
+sem_t empty; // semaphore for count when empty
+
+// Producer thread function
+void *producer(void *param) {
+  // Each producer insert its own ID into the buffer
+  // For example, thread 1 will insert 1, thread 2 will insert 2, and so on.
+  buffer_item item = *((int *)param);
+
+  while (true) {
+    /* sleep for a random period of time */
+    usleep(rand() % 1000000);
+    sem_wait(&empty);           // wait for empty opening
+    pthread_mutex_lock(&mutex); // Lock for critical section
+    if (buffer.insert_item(item)) {
+      cout << "Producer " << item << ": Inserted item " << item << endl;
+      buffer.print_buffer();
+    } else {
+      cout << "Producer error condition" << endl; // shouldn't come here
+    }                                             // End of if/else
+    pthread_mutex_unlock(&mutex); // Unlock after critical section
+    sem_post(&full);              // send when buffer is full
+  }
+}
+
+// Consumer thread function
+void *consumer(void *param) {
+  buffer_item item;
+
+  while (true) {
+    /* sleep for a random period of time */
+    usleep(rand() % 1000000);
+    sem_wait(&full);            // wait for when full
+    pthread_mutex_lock(&mutex); // Lock for critical section
+    if (buffer.remove_item(&item)) {
+      cout << "Consumer " << item << ": Removed item " << item << endl;
+      buffer.print_buffer();
+    } else {
+      cout << "Consumer error condition" << endl; // shouldn't come here
+    }                                             // End of if/else block
+    pthread_mutex_unlock(&mutex); // Unlock after critical section
+    sem_post(&empty);             // send when buffer is not at max size
+  }
+}
+
+int main(int argc, char *argv[]) {
+  /* 1. Get command line arguments argv[1],argv[2],argv[3] */
+
+  // all three command line arguments
+  int sleepPeriod = atoi(argv[1]);
+  int producersNum = atoi(argv[2]);
+  int consumersNum = atoi(argv[3]);
+
+  /* 2. Initialize buffer and synchronization primitives */
+
+  pthread_mutex_init(&mutex, NULL);
+  sem_init(&full, 0, 0); // full semaphore initalized to 0
+  sem_init(&empty, 0,
+           5); // empty semaphore initalized to the size of the buffer(5)
+
+  pthread_t producerThread[producersNum]; // producer thread
+  pthread_t consumerThread[consumersNum]; // consumer thread
+
+  /* 3. Create producer thread(s).
+   * You should pass an unique int ID to each producer thread, starting from 1
+   * to number of threads */
+  for (int i = 0; i < producersNum; i++) {
+    pthread_create(&producerThread[i], NULL, &producer, (void *)&i);
+  }
+
+  /* 4. Create consumer thread(s) */
+  for (int i = 0; i < consumersNum; i++) {
+    pthread_create(&consumerThread[i], NULL, &consumer, NULL);
+  }
+  /* 5. Main thread sleep */
+  // sleep for a period of time
+  sleep(sleepPeriod);
+
+  /* 6. Exit */
+  return 0;
+}
